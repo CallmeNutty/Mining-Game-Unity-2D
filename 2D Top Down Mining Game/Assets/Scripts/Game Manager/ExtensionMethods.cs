b41d35e04e276a1835ca107bde7bdf1cdee31c0d
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-02-11 18:10:46 +0000

    Initial Commit after Creation
    Added:
    -Movement
    -Sprites
    -Background Tiles

---
+++ 2D Top Down Mining Game/Assets/Scripts/Game Manager/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static void LookAt2D(this Transform transform, Vector2 target)
+    {
+        Vector2 current = transform.position;
+        var direction = target - current;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
---
+++ 2D Top Down Mining Game/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
+        ExtensionMethods.LookAt2D(transform, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.AddRelativeForce(new Vector2(0, 5), ForceMode2D.Force);
+        }
+	}
+}
