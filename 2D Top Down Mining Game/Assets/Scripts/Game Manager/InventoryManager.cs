e97f8e400ac81bd3dcac776d615b154022e36f4d
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-02-11 21:51:43 +0000

    Began working on Minerals and Inventory
    Added:
    -Minerals
    -Framework for future minerals
    -Began working on a flexible inventory system

--- 2D Top Down Mining Game/Assets/Scripts/Game Manager/InventoryManager.cs
+++ 2D Top Down Mining Game/Assets/Scripts/Game Manager/InventoryManager.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public static Dictionary<string, int> Inventory = new Dictionary<string, int>();
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        Inventory.Add("Gulm", 0);
+        Inventory.Add("Genrite", 0);
+        Inventory.Add("Carocil", 0);
+        Inventory.Add("Osmanium", 0);
+        Inventory.Add("Glorium", 0);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        print(Inventory["Gulm"]);
 	}
 }
---
+++ 2D Top Down Mining Game/Assets/Scripts/Game World/Minerals.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minerals : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            InventoryManager.Inventory[mineral.name] += 1;
+            Destroy(coll.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    [System.Serializable]
+    public class Mineral
+    {
+        public string name;
+        public int health;
+    }
+
+    public Mineral mineral;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
