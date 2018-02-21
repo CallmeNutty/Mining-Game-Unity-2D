af4a37ee5d10175ba35a892f332fa6ea4fea9b5a
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-02-21 18:37:36 +0000

    Company Name on Inventory Screen

--- 2D Top Down Mining Game/Assets/Scripts/Game Manager/Money.cs
+++ 2D Top Down Mining Game/Assets/Scripts/Game Manager/Money.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Money : MonoBehaviour {
+public class Money : MonoBehaviour
+{
 
     [SerializeField]
     private Text moneyText;
