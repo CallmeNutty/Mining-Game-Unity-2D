2badacabedaf9707f7e9428dc4613e2fb2aa4d39
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-03-05 19:06:47 +0000

    All minerals added

--- 2D Top Down Mining Game/Assets/Scripts/Game World/Planets.cs
+++ 2D Top Down Mining Game/Assets/Scripts/Game World/Planets.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class Planets : MonoBehaviour
 {
     //Declare Variables
+    private bool inZone;
+
     [SerializeField]
     private GameObject eKey;
     [SerializeField]
@@ -14,13 +16,10 @@ public class Planets : MonoBehaviour
     //Entered Planet Zone
     void OnTriggerStay2D(Collider2D coll)
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            planetMenu.SetActive(true); //Show planet UI
-            Time.timeScale = 0;
-        }
+        inZone = true;
+
         //If player has entered the zone
-        else if (coll.tag == "Player")
+        if (coll.tag == "Player")
         {
             InventoryManager.eKeyFree = false; //Hide other "e key" prompts
             eKey.SetActive(true); //Show this prompt
@@ -30,10 +29,30 @@ public class Planets : MonoBehaviour
     //Exited Planet Zone
     void OnTriggerExit2D(Collider2D coll)
     {
+        inZone = false;
+
         if (coll.tag == "Player")
         {
             InventoryManager.eKeyFree = true; //Show other "e key" prompts
             eKey.SetActive(false); //Hide this prompt
+        }
+    }
+
+    void Update()
+    {
+        //If E is down and menu is not currently open
+        if (Input.GetKeyDown(KeyCode.E) && inZone == true && planetMenu.activeInHierarchy == false)
+        {
+            print("menu no");
+            planetMenu.SetActive(true); //Show planet UI
+            Time.timeScale = 0;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.E) && inZone == true && planetMenu.activeInHierarchy == true)
+        {
+            print("menu yes");
+            planetMenu.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 }
