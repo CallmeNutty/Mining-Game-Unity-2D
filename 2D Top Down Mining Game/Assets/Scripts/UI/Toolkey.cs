3c03efa63d92a4a88b4fdc39e1e63547c1903960
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-04-02 12:08:00 +0100

    Can now click and buy items

--- 2D Top Down Mining Game/Assets/Scripts/UI/Toolkey.cs
+++ 2D Top Down Mining Game/Assets/Scripts/UI/Toolkey.cs
@@ -12,7 +12,12 @@ public class Toolkey : MonoBehaviour
     private Text itemName;
     [SerializeField]
     private Text cost;
+
     public ItemDatabase itemDatabase;
+    [SerializeField]
+    private Money money;
+    [SerializeField]
+    private InventoryManager inventoryManager;
 	
 	// Update is called once per frame
 	void Update ()
@@ -54,6 +59,27 @@ public class Toolkey : MonoBehaviour
 
     public void BuyItem()
     {
-        print(gameObject.name);
+        //Get item that is present here
+        Planet thisItem = ExtensionMethods.FindBySprite(gameObject.transform.GetChild(0).GetComponent<Image>().sprite, itemDatabase).eachPlanet.Find(x => x.ID == Planets.currentPlanet.ID);
+
+        money.money -= thisItem.price;
+        thisItem.amount--;
+    }
+
+    public void SellItem()
+    {
+        if (gameObject.transform.GetChild(0).GetComponent<Image>().sprite != null)
+        {
+            //Get item that is present here
+            Planet thisItem = ExtensionMethods.FindBySprite(gameObject.transform.GetChild(0).GetComponent<Image>().sprite, itemDatabase).eachPlanet.Find(x => x.ID == Planets.currentPlanet.ID);
+
+            money.money += thisItem.price;
+            //Remove 1 from Inventory
+            inventoryManager.Inventory.Find(x => x == ExtensionMethods.FindBySprite(gameObject.transform.GetChild(0).GetComponent<Image>().sprite, itemDatabase)).amount--;
+        }
+        else
+        {
+            print("Empty Click! No Sell for you :)");
+        }
     }
 }
